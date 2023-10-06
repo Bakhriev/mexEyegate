1a2208822133ff83d56c0ff04fbe2cf01af942f5
Bakhriev <legendofpain5112@gmail.com>
2023-10-06 19:38:16 +0500

    last

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,12 +1,16 @@
-import {burgerMenu} from './functions/burger'
 import './functions/scrollToTop'
-
-burgerMenu()
 
 const toggleButtons = document.querySelectorAll(
 	'.mobile-list__item-has-children'
 )
 const subMenus = document.querySelectorAll('.mobile-submenu')
+const mobileClose = document.querySelector('.mobile-menu__btn')
+const mobileMenu = document.querySelector('.mobile-menu')
+
+const burger = document.querySelector('.burger')
+const overlay = document.querySelector('.overlay')
+const body = document.querySelector('body')
+const elements = [burger, mobileMenu, overlay]
 
 toggleButtons.forEach((button, index) => {
 	button.addEventListener('click', e => {
@@ -33,3 +37,29 @@ const resetActiveToggle = () => {
 		btn.classList.remove('active')
 	})
 }
+
+mobileClose.addEventListener('click', () => {
+	resetActiveSubmenu()
+	resetActiveToggle()
+	toggleActiveClass()
+})
+
+const toggleActiveClass = () => {
+	elements.forEach(element => element.classList.toggle('active'))
+	body.classList.toggle('disable-scroll')
+}
+
+export const burgerMenu = () => {
+	burger.addEventListener('click', toggleActiveClass)
+
+	overlay.addEventListener('click', toggleActiveClass)
+
+	window.addEventListener('resize', () => {
+		const {innerWidth} = window
+		if (innerWidth > 991.98) {
+			elements.forEach(element => element.classList.remove('active'))
+			body.classList.remove('disable-scroll')
+		}
+	})
+}
+burgerMenu()
