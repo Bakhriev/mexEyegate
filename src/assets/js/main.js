7994bbc3f9701f86d54f62f18650a867b59c5cb3
Bakhriev <legendofpain5112@gmail.com>
2023-10-06 18:34:01 +0500

    кое как

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -2,3 +2,34 @@ import {burgerMenu} from './functions/burger'
 import './functions/scrollToTop'
 
 burgerMenu()
+
+const toggleButtons = document.querySelectorAll(
+	'.mobile-list__item-has-children'
+)
+const subMenus = document.querySelectorAll('.mobile-submenu')
+
+toggleButtons.forEach((button, index) => {
+	button.addEventListener('click', e => {
+		resetActiveSubmenu()
+		resetActiveToggle()
+		toggleButtons[index].classList.toggle('active')
+		subMenus[index].classList.toggle('active')
+
+		if (e.target.className === 'mobile-submenu__btn') {
+			resetActiveSubmenu()
+			resetActiveToggle()
+		}
+	})
+})
+
+const resetActiveSubmenu = () => {
+	subMenus.forEach(menu => {
+		menu.classList.remove('active')
+	})
+}
+
+const resetActiveToggle = () => {
+	toggleButtons.forEach(btn => {
+		btn.classList.remove('active')
+	})
+}
